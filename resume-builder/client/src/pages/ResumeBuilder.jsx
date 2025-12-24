6870f7927bf2644507153583404c531228acaf49
MSCPerera <164726262+MSCPerera@users.noreply.github.com>
2025-12-24 23:55:22 +0530

    Update Resume Builder

--- resume-builder/client/src/pages/ResumeBuilder.jsx
+++ resume-builder/client/src/pages/ResumeBuilder.jsx
@@ -3,6 +3,7 @@ import { Link, useParams } from 'react-router-dom'
 import { dummyResumeData } from '../assets/assets'
 import { ArrowLeftIcon, Briefcase, ChevronLeft, ChevronRight, FileText, FolderIcon, GraduationCap, Sparkles, User } from 'lucide-react'
 import PersonalInfoForm from '../components/PersonalInfoForm'
+import ResumePreview from '../components/ResumePreview'
 
 const ResumeBuilder = () => {
 
@@ -101,8 +102,12 @@ const ResumeBuilder = () => {
           </div>
 
           {/* Right Panel - Resume Preview  */}
-          <div>
-            
+          <div className='lg:col-span-7 max-lg:mt-6'>
+            <div>
+              {/* --- buttons --- */}
+            </div>
+
+            <ResumePreview data={resumeData} template={resumeData.template} accentColor={resumeData.accent_color}/>
           </div>
 
         </div>
