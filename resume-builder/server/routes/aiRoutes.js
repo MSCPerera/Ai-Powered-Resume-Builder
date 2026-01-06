13f51764a012eaf56b4561d41f37edc94fa5085e
MSCPerera <164726262+MSCPerera@users.noreply.github.com>
2026-01-06 23:51:55 +0530

    Update Ai Routes

--- resume-builder/server/routes/aiRoutes.js
+++ resume-builder/server/routes/aiRoutes.js
@@ -1,12 +1,11 @@
 import express from 'express';
 import protect from '../middlewares/authMiddleware.js';
-import { enhanceJobDescription, enhanceProfessionaalSummery } from '../controllers/aiController.js';
-import { updateResume } from '../controllers/resumeController.js';
+import { enhanceJobDescription, enhanceProfessionaalSummery, uploadResume } from '../controllers/aiController.js';
 
 const aiRouter = express.Router();
 
 aiRouter.post('/enhance-pro-sum', protect, enhanceProfessionaalSummery)
 aiRouter.post('/enhance-job-desc', protect, enhanceJobDescription)
-aiRouter.post('/upload-resume', protect, updateResume)
+aiRouter.post('/upload-resume', protect, uploadResume)
 
 export default aiRouter
